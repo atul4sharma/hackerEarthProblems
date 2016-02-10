f9a5066ab7cfd82971edbd740d724f11100f3cf5
atul sharma <atulsharma406@gmail.com>
2016-02-10 20:26:46 +0530

    add anagrams_revisited.cpp

--- anagrams_revisited.cpp
+++ anagrams_revisited.cpp
@@ -1,66 +1,24 @@
-#include <string>
-#include <iostream>
-#include <vector>
-
+#include<bits/stdc++.h>
 using namespace std;
-
-int compare(string,string);
-
 int main()
 {
- int n,max=0;
- 
- int count[51];
- for(int i=0;i<52;i++)
-     count[i]=0;
- cin>>n;
- vector<string> v[52];
- while(n--)
- {  string s;
- 
- 	cin>>s;
-        v[s.length()].push_back(s);
-      //  cout<<s.length()<<" ";
-    for(int i=0;i<v[s.length()].size();i++)
-    {
-        if(compare(v[s.length()][i],s)==1)
-        {count[s.length()]+=1;
-            break;}
-    }
-    
-		
- }
- for(int i=0;i<50;i++)
- {
- 	if(count[i]>max)
- 	max=count[i];
- }
- 
- cout<<max<<endl;
-    return 0;
-}
-
-
-int compare(string a,string b)
-{   int k;
-    for(int i=0;i<a.length();i++)
-    {   k=1;
-        for(int j=0;j<b.length();j++)
-        {   if(a[i]==b[j])
-            {
-                k=0; 
-                break;
-            }
-            
-        }
-        if(k==1)
-        {break;
-          }
-            
-    }
-    
-    if(k==1)
-        return 0;//means string has different alphabets
-    else
-        return 1;//means string is permuted
+	map<string, int> M;
+	map<string, int> :: iterator it;
+	int N;
+	cin>>N;
+	for(int i=0;i<N;i++)
+	{
+		string s;
+		cin>>s;
+		sort(s.begin(),s.end());
+		M[s]+=1;
+	}
+	int max=-1;
+	for(it=M.begin();it!=M.end();it++)
+	{
+		if(it->second>max)
+			max=it->second;
+	}
+	cout<<max<<endl;
+	return 0;
 }
