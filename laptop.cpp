9332863350fde4dec7d0696991554537e7475485
atul sharma <atulsharma406@gmail.com>
2016-02-10 20:29:38 +0530

    modified laptop.cpp MAP used

--- laptop.cpp
+++ laptop.cpp
@@ -1,70 +1,43 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
-
 int main()
-{   
-    vector<string> v;
-
-        unsigned long int n;
-        int k;
-        vector<string> result;
-        cin>>n;
-        unsigned long int count[n];
-        for(unsigned long int i=0;i<n;i++)
-            count[i]=0;
-        while(n--)
-        {   string s;
-            cin>>s;
-            k=1;
-            for(int i=0;i<v.size();i++)
-            {
-                if(v[i]==s)
-                {
-                    count[i]=count[i]+1;
-                    k=0;
-                    break;
-                }
-            }
-            if(k==1)
-            { v.push_back(s);
-                count[v.size()-1]+=1;
-            }
-            
-        }
-        unsigned long int maxVal=0;
-        vector<unsigned long int> maxIndex;
-        
-        
-        for(unsigned long int i=0;i<v.size();i++)
-        {   if(count[i]==maxVal)
-            {
-                //result.push_back(v[i]);
-                maxIndex.push_back(i);
-            }
-            else if(count[i]>maxVal)
-            {
-               // result.clear();
-                maxIndex.clear();
-               // result.push_back(v[i]);
-                maxIndex.push_back(i);
-                maxVal=count[i];
-            }
-            
-           
-        }
-    
-    for(unsigned long int i=0;i<maxIndex.size();i++)
-        result.push_back(v[maxIndex[i]]);
-    
-    sort(result.begin(),result.end());
-    
-    for(unsigned long int i=0;i<result.size();i++)
-        cout<<result[i]<<endl;
-    
-    return 0;
-    
+{
+	int N;
+	cin>>N;
+	vector<string> V;
+	map<string, int> M;
+	map<string, int> :: iterator it;
+	for(int i=0;i<N;i++)
+	{
+		string S;
+		cin>>S;
+		if(M.find(S)==M.end())
+		{
+			M[S]=1;
+		}
+		else
+		{
+			M[S]++;
+		}
+	}
+	int maxx=-1;
+	string ans;
+	for(it=M.begin();it!=M.end();++it)
+	{
+		if(it->second> maxx)
+		{
+			maxx=it->second;
+			ans=it->first;
+		}
+	}
+	for(it=M.begin();it!=M.end();++it)
+	{
+		if(it->second== maxx)
+		{
+			V.push_back(it->first);
+		}
+	}
+	sort(V.begin(), V.end());
+	cout<<V[0]<<endl;
+	return 0;
 }
